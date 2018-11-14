a60048e06ea00ee027b5310016ecff78e6130596
Octavio2121 <tavo._22@hotmail.com>
2018-11-14 13:24:12 -0600

    prueba

---
+++ Taller.java
@@ -0,0 +1,16 @@
+
+package tallerciitec;
+/**
+ *
+ * @author antonio072 del Tecno
+ * Clase de Java creada para el taller de Git y Github
+ */
+public class TallerCIITEC {
+    System.exit(1);
+    public static void main(String[] args) {
+      int[]variable={1,2,3,4,5,6,7};
+      foreach(int i : variable){
+        System.out.println(i+" ");
+      }
+    }
+}
